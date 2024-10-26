a7932dbcb7f4130e55dbff2e38a90876912bf386
qachar hosna <qachar.hosna@gmail.com>
2024-10-26 19:23:37 +0200

    Ajout dernière modifications

--- backend/middleware/sharp.js
+++ backend/middleware/sharp.js
@@ -13,23 +13,17 @@ module.exports = async (req, res, next) => {
         const isWebP = ext === '.webp'; // Vérifie si l'image est déjà en WebP
         let optimizedImageName, optimizedImagePath;
 
-        if (isWebP) {
-            // Si c'est déjà un fichier WebP, renommez-le
-            optimizedImageName = path.basename(originalImagePath);
-            optimizedImagePath = originalImagePath; // Pas besoin de le convertir
-        } else {
-            // Si ce n'est pas WebP, créez un nouveau nom et chemin pour l'image optimisée
-            optimizedImageName = `optimized_${path.basename(originalImagePath, ext)}.webp`; 
-            optimizedImagePath = path.join('images', optimizedImageName);
+        // Même si l'image est déjà en WebP, nous devons vérifier la taille et la redimensionner si nécessaire
+        optimizedImageName = `optimized_${path.basename(originalImagePath, ext)}.webp`; 
+        optimizedImagePath = path.join('images', optimizedImageName);
 
-            // Utilisez Sharp pour redimensionner et convertir l'image
-            await sharp(originalImagePath)
-                .resize({ width: 400, height: 300  }) // Redimensionnez à une largeur de 500px
-                .webp({ quality: 80 }) // Convertissez en WebP avec une qualité de 80%
-                .toFile(optimizedImagePath); // Enregistrez le fichier
-        }
+        // Utilisez Sharp pour redimensionner l'image (même si elle est déjà en WebP)
+        await sharp(originalImagePath)
+            .resize({  fit: sharp.fit.fill }) // Redimensionnez à une taille spécifiée
+            .webp({ quality: 80 }) // Convertissez en WebP ou gardez WebP avec la qualité spécifiée
+            .toFile(optimizedImagePath); // Enregistrez l'image optimisée
 
-        // Supprimez l'image originale
+        // Supprimez l'image originale après la conversion
         fs.unlink(originalImagePath, (error) => {
             if (error) {
                 console.error("Impossible de supprimer l'image originale :", error);
